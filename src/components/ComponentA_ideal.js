4374e0e18c537374926120f2b7d8a24acd786f01
IreneMaryanne <irenemaryanne_m@hotmail.com>
2019-10-03 16:27:47 +0800

    Create ComponentA_ideal.js

---
+++ src/components/ComponentA_ideal.js
@@ -0,0 +1,17 @@
+var ComponentA = {
+    props: {
+        color: {
+          type: String,
+          default: "black"
+        }
+    },
+    methods: {
+        /** @public */
+        method1: function(e){
+            console.log("This is method1");
+        }
+    },
+    template: `<div :style="{color: color}">This is componentA<div>`
+}
+
+if (typeof module !== 'undefined' && module.exports) module.exports = ComponentA;
